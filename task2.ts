65aee7c15ea28acee9ec36a14c0fc57b806eec3c
mjaqamy <mjaqamy@gmail.com>
2026-01-26 21:20:32 +0300

    feat: resolve task2

--- task2.ts
+++ task2.ts
@@ -1,7 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const num1: number = 0.7767667;
 const num2: number = 3.2232332;
 
 //Написать программу, которая выведет целочисленную сумму двух чисел, для оптимизации - сделать проверку на целое число
 
 const newNum = num1 + num2;
+
+if (Number.isInteger(newNum)) {
+  console.log(newNum);
+} else {
+  console.log(newNum.toFixed());
+}
