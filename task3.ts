d9f30abaaf35d390c21665ad1b1f45ba5e44e0db
mjaqamy <mjaqamy@gmail.com>
2026-01-27 22:38:28 +0300

    feat: task3

--- task3.ts
+++ task3.ts
@@ -1,2 +1,13 @@
 //Вывести сегодняшнюю дату в формате:
 //День/Месяц/Год Часы:Минуты:Секунду - Например 02/05/2026 22:15:30
+
+const dateNow = new Date();
+
+const day = dateNow.getDate().toString().padStart(2, "0");
+const month = (dateNow.getMonth() + 1).toString().padStart(2, "0");
+const year = dateNow.getFullYear().toString().padStart(2, "0");
+const hours = dateNow.getHours().toString().padStart(2, "0");
+const min = dateNow.getMinutes().toString().padStart(2, "0");
+const sec = dateNow.getSeconds().toString().padStart(2, "0");
+
+console.log(`${day}/${month}/${year} ${hours}:${min}:${sec}`);
