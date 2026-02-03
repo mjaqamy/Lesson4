452799f0c1c34b4a980f3e559bd8876e21054aba
mjaqamy <mjaqamy@gmail.com>
2026-02-03 19:55:40 +0300

    fix: refactoring task4

--- task4.ts
+++ task4.ts
@@ -5,16 +5,25 @@ const order = "Order#1456; date=2026-01-26 09:07:05; amount=15.3";
 
 const numOrder = order.slice(order.indexOf("#") + 1, order.indexOf(";"));
 
+const dateStart = order.indexOf("date=") + 5;
+const dateEnd = order.indexOf("; amount");
+const dateString = order.slice(dateStart, dateEnd);
+const date = new Date(dateString.replace(" ", "T"));
+const day = String(date.getDate()).padStart(2, "0");
+const month = String(date.getMonth() + 1).padStart(2, "0");
+const year = date.getFullYear();
+
+const hours = String(date.getHours()).padStart(2, "0");
+const minutes = String(date.getMinutes()).padStart(2, "0");
 const newDate = order.slice(order.indexOf("=") + 1, order.indexOf("=") + 11);
 
-const elementsOfDate = newDate.split("-");
-const day = elementsOfDate[2];
-const months = elementsOfDate[1];
-const year = elementsOfDate[0];
+// const elementsOfDate = newDate.split("-");
+// const day = elementsOfDate[2];
+// const months = elementsOfDate[1];
+// const year = elementsOfDate[0];
 
-const time = order.slice(order.indexOf("=") + 12, order.indexOf(":") + 3);
+// const time = order.slice(order.indexOf("=") + 12, order.indexOf(":") + 3);
 const amount = Math.ceil(Number(order.slice(order.indexOf("amount=") + 7)));
 
-const NewOrder =
-  "Заказ № " + numOrder + " от " + `${day}/${months}/${year} ` + time + " на сумму " + `${amount}` + " рублей";
+const NewOrder = `Заказ № ${numOrder} от ${day}/${month}/${year} ${hours}:${minutes} на сумму ${amount} рублей`;
 console.log(NewOrder);
